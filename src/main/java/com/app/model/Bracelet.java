9fb4d2d1270dafbdbc34125556f2a301c4d17461
Evita <stoukly@inbox.lv>
2023-11-08 19:43:15 +0200

    Created objects

---
+++ src/main/java/com/app/model/Bracelet.java
@@ -0,0 +1,139 @@
+package com.app.model;
+
+public class Bracelet {
+    long id;
+    String type;
+    String designedFor;
+    String brand;
+    String collection;
+    int length;
+    String material;
+    int finenessOfMetal;
+    String colorOfMetal;
+    double metalWeight;
+    boolean gem;
+    double gemWeight;
+    String colorOfGem;
+    String closure;
+    float price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDesignedFor() {
+        return designedFor;
+    }
+
+    public void setDesignedFor(String designedFor) {
+        this.designedFor = designedFor;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getFinenessOfMetal() {
+        return finenessOfMetal;
+    }
+
+    public void setFinenessOfMetal(int finenessOfMetal) {
+        this.finenessOfMetal = finenessOfMetal;
+    }
+
+    public String getColorOfMetal() {
+        return colorOfMetal;
+    }
+
+    public void setColorOfMetal(String colorOfMetal) {
+        this.colorOfMetal = colorOfMetal;
+    }
+
+    public double getMetalWeight() {
+        return metalWeight;
+    }
+
+    public void setMetalWeight(double metalWeight) {
+        this.metalWeight = metalWeight;
+    }
+
+    public boolean isGem() {
+        return gem;
+    }
+
+    public void setGem(boolean gem) {
+        this.gem = gem;
+    }
+
+    public double getGemWeight() {
+        return gemWeight;
+    }
+
+    public void setGemWeight(double gemWeight) {
+        this.gemWeight = gemWeight;
+    }
+
+    public String getColorOfGem() {
+        return colorOfGem;
+    }
+
+    public void setColorOfGem(String colorOfGem) {
+        this.colorOfGem = colorOfGem;
+    }
+
+    public String getClosure() {
+        return closure;
+    }
+
+    public void setClosure(String closure) {
+        this.closure = closure;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
